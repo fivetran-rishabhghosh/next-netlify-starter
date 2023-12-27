6872198501acecaad404f7ca5a3d50400a28758a
fivetran-rishabhghosh <105623976+fivetran-rishabhghosh@users.noreply.github.com>
2023-12-27 20:01:03 +0530

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -8,38 +8,34 @@ export default function Home() {
       <Head>
           <script type='text/javascript'>
 
-           function(p,l,o,w,i,n,g) {
-                if(!p[i]) {
-                    p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
-                    p.GlobalSnowplowNamespace.push(i);p[i]=function() {
-                        (p[i].q=p[i].q||[]).push(arguments)
-                    }
-                    ;p[i].q=p[i].q||[];
-                    n=l.createElement(o);
-                    g=l.getElementsByTagName(o)[0];
-                    n.async=1;
-                    n.src=w;
-                    g.parentNode.insertBefore(n,g)
-                }}(window,document,"script",'//cdn.jsdelivr.net/gh/snowplow/sp-js-assets@2.15.0/sp.js',"snowplow");
-
-              // Initialize a tracker
-              window.snowplow('newTracker', 'cf', 'webhooks.fivetran.com/snowplow/ee56030d-cc8b-4998-b101-82ac53a36ece', {
-                appId:  'test-rishabh-snowplow-netlify' ,
-                cookieDomain: null,
-                post: true
-              });
-
-              window.snowplow('trackPageView');
+;(function(p,l,o,w,i,n,g){
+  if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
+  p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
+  };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
+  n.src=w;g.parentNode.insertBefore(n,g)}
+}(
+  window,
+  document,
+  "script",
+  "//cdn.jsdelivr.net/gh/snowplow/sp-js-assets@2.15.0/sp.js",
+  "snowplow")
+);
 
 
-            snowplow_name_here("newTracker", "sp", "webhooks.fivetran.com/snowplow/ee56030d-cc8b-4998-b101-82ac53a36ece", {
-                appId: "test-rishabh-snowplow-netlify",
-                platform: "web",
-                contexts: {
-                    webPage: true,
-                    performanceTiming: true
-                }
-            });
+snowplow("newTracker", "try", "webhooks.fivetran.com/snowplow/ee56030d-cc8b-4998-b101-82ac53a36ece", {
+  appId: "test-rishabh-snowplow-netlify", // you can specify your own app name here
+  platform: "web",
+
+  contexts: {
+    webPage: true, // this sets a unique id for each page view
+    performanceTiming: true // this captures performance metrics like load times
+  }
+});
+
+// send first heartbeat after 10 seconds, and every 10 seconds thereafter
+snowplow("enableActivityTracking", { minimumVisitLength: 10, heartbeatDelay: 10 });
+snowplow("enableLinkClickTracking");
+snowplow("trackPageView");
 
           </script>
 
