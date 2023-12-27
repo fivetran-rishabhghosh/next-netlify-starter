f9046b7382e1a4e65a914517011a123877c7631a
fivetran-rishabhghosh <105623976+fivetran-rishabhghosh@users.noreply.github.com>
2023-12-27 19:54:43 +0530

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -8,19 +8,39 @@ export default function Home() {
       <Head>
           <script type='text/javascript'>
 
-              ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
-              p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments)
-              };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1;
-              n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,'script','//cdn.jsdelivr.net/gh/snowplow/sp-js-assets@2.15.0/sp.js','snowplow'));
+           ;(function(p,l,o,w,i,n,g) {
+                if(!p[i]) {
+                    p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[];
+                    p.GlobalSnowplowNamespace.push(i);p[i]=function() {
+                        (p[i].q=p[i].q||[]).push(arguments)
+                    }
+                    ;p[i].q=p[i].q||[];
+                    n=l.createElement(o);
+                    g=l.getElementsByTagName(o)[0];
+                    n.async=1;
+                    n.src=w;
+                    g.parentNode.insertBefore(n,g)
+                }}(window,document,"script",'//cdn.jsdelivr.net/gh/snowplow/sp-js-assets@2.15.0/sp.js',"snowplow"));
 
               // Initialize a tracker
-              window.snowplow('newTracker', 'cf', 'webhooks.fivetran.com/snowplow/<endpoint_ID>', {
+              window.snowplow('newTracker', 'cf', 'webhooks.fivetran.com/snowplow/ee56030d-cc8b-4998-b101-82ac53a36ece', {
                 appId:  'test-rishabh-snowplow-netlify' ,
                 cookieDomain: null,
                 post: true
               });
 
               window.snowplow('trackPageView');
+
+
+            snowplow_name_here("newTracker", "sp", "webhooks.fivetran.com/snowplow/ee56030d-cc8b-4998-b101-82ac53a36ece", {
+                appId: "test-rishabh-snowplow-netlify",
+                platform: "web",
+                contexts: {
+                    webPage: true,
+                    performanceTiming: true
+                }
+            });
+
           </script>
 
         <title>Next.js Starter!</title>
@@ -38,12 +58,3 @@ export default function Home() {
     </div>
   )
 }
-
-snowplow_name_here("newTracker", "sp", "webhooks.fivetran.com/snowplow/ee56030d-cc8b-4998-b101-82ac53a36ece", {
-    appId: "test-rishabh-snowplow-netlify",
-    platform: "web",
-    contexts: {
-        webPage: true,
-        performanceTiming: true
-    }
-});
